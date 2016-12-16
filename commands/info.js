0f531bb91fd477ed479d4360bb224939ae2d8896
ArsenArsen <ArsenArsen@users.noreply.github.com>
2016-12-16 17:58:06 +0100

    Fix a typo in @Lunar-Skies' name

--- commands/info.js
+++ commands/info.js
@@ -2,7 +2,7 @@
 
 class Info {
     process(msg, args){
-        msg.channel.createMessage('Hi! I am Hamster! I am a self-host bot made by Luanr-Skies and ArsenArsen!');
+        msg.channel.createMessage('Hi! I am Hamster! I am a self-host bot made by Lunar-Skies and ArsenArsen!');
     }
 }
 module.exports = Info;
